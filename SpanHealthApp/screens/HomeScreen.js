edd57e6bcdfacd72319668aff53f610606d67a98
MissDavinderKaur <miss_davinder_kaur@hotmail.co.uk>
2019-09-25 20:59:58 +0100

    Some of the Home screen is in place

--- SpanHealthApp/screens/HomeScreen.js
+++ SpanHealthApp/screens/HomeScreen.js
@@ -16,55 +16,38 @@ export default function HomeScreen() {
   return (
     <View style={styles.container}>
       <ScrollView
-        style={styles.container}
-        contentContainerStyle={styles.contentContainer}>
-        <View style={styles.welcomeContainer}>
-          <Image
-            source={
-              __DEV__
-                ? require('../assets/images/robot-dev.png')
-                : require('../assets/images/robot-prod.png')
-            }
-            style={styles.welcomeImage}
-          />
-        </View>
-
-        <View style={styles.getStartedContainer}>
-          <DevelopmentModeNotice />
-
-          <Text style={styles.getStartedText}>Get started by opening</Text>
-
-          <View
-            style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-            <MonoText>screens/HomeScreen.js</MonoText>
+      style={styles.container}
+      contentContainerStyle={styles.contentContainer}>
+        <View style={styles.getStartedOptionsContainer}>
+          <Text style={styles.foodHeading}>Food</Text>
+          <View style={styles.getStartedOptions}>
+            <View>
+              <Image
+              source={require('../assets/images/blog.jpg')}
+              style={styles.optionImage}
+              />
+              <Text style={styles.optionText}>Blog</Text>
+            </View>
+            <View>
+              <Image
+              source={require('../assets/images/recipes.jpg')}
+              style={styles.optionImage}
+              />
+              <Text style={styles.optionText}>Recipes</Text>
+            </View>
           </View>
-
-          <Text style={styles.getStartedText}>
-            Change this text and your app will automatically reload.
-          </Text>
         </View>
 
-        <View style={styles.helpContainer}>
-          <TouchableOpacity onPress={handleHelpPress} style={styles.helpLink}>
-            <Text style={styles.helpLinkText}>
-              Help, it didn’t automatically reload!
-            </Text>
-          </TouchableOpacity>
+        <View style={styles.blogFeed}>
+          <View style={styles.blog}>
+            <Image
+            source={require('../assets/images/blogImage1.jpg')}
+            style={styles.blogImage}
+            />
+            <Text style={styles.blogText}>How to interpret HbA1c test data</Text>
+          </View>
         </View>
       </ScrollView>
-
-      <View style={styles.tabBarInfoContainer}>
-        <Text style={styles.tabBarInfoText}>
-          This is a tab bar. You can edit it in:
-        </Text>
-
-        <View
-          style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-          <MonoText style={styles.codeHighlightText}>
-            navigation/MainTabNavigator.js
-          </MonoText>
-        </View>
-      </View>
     </View>
   );
 }
@@ -73,88 +56,54 @@ HomeScreen.navigationOptions = {
   header: null,
 };
 
-function DevelopmentModeNotice() {
-  if (__DEV__) {
-    const learnMoreButton = (
-      <Text onPress={handleLearnMorePress} style={styles.helpLinkText}>
-        Learn more
-      </Text>
-    );
-
-    return (
-      <Text style={styles.developmentModeText}>
-        Development mode is enabled: your app will be slower but you can use
-        useful development tools. {learnMoreButton}
-      </Text>
-    );
-  } else {
-    return (
-      <Text style={styles.developmentModeText}>
-        You are not in development mode: your app will run at full speed.
-      </Text>
-    );
-  }
-}
-
-function handleLearnMorePress() {
-  WebBrowser.openBrowserAsync(
-    'https://docs.expo.io/versions/latest/workflow/development-mode/'
-  );
-}
-
-function handleHelpPress() {
-  WebBrowser.openBrowserAsync(
-    'https://docs.expo.io/versions/latest/workflow/up-and-running/#cant-see-your-changes'
-  );
-}
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
   },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: 'center',
-  },
   contentContainer: {
     paddingTop: 30,
   },
-  welcomeContainer: {
+  getStartedOptionsContainer: {
     alignItems: 'center',
-    marginTop: 10,
-    marginBottom: 20,
+    marginTop: 20,
   },
-  welcomeImage: {
-    width: 100,
-    height: 80,
-    resizeMode: 'contain',
-    marginTop: 3,
-    marginLeft: -10,
-  },
-  getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
-  },
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
-  },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
+  foodHeading: {
+    fontSize: 40,
+    color: '#8ECFCF',
     textAlign: 'center',
+  },
+  getStartedOptions: {
+    flex: 1,
+    flexDirection: 'row'
+  },
+  optionImage: {
+    width: 100,
+    height: 100,
+    marginTop: 50,
+    marginLeft: 30,
+    marginRight: 30,
+    borderRadius: 20,
+  },
+  optionText: {
+    fontSize: 20,
+    marginTop: 10,
+    color: '#8ECFCF',
+    textAlign: 'center',
+  },
+  blogFeed: {
+    alignItems: 'center',
+    marginTop: 5,
+  },
+  blog: {
+
+  },
+  blogImage: {
+    width: 300,
+    resizeMode: 'center',
+  },
+  blogText: {
+
   },
   tabBarInfoContainer: {
     position: 'absolute',
@@ -175,24 +124,5 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#fbfbfb',
     paddingVertical: 20,
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
-  },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
   },
 });
